97adf03c03b34af59b8dc6a1966436cab526f91c
Innei <tukon479@gmail.com>
2020-07-24 21:07:40 +0800

    Updates

---
+++ precache-manifest.a276f655f294634e7aa154ec4a6ec0d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d517b67f0beae3764dd6a163a4de18c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "265a3b0486e1884463bb",
+    "url": "./static/css/main.c4472847.chunk.css"
+  },
+  {
+    "revision": "f0ba4f16937ca56d5fc0",
+    "url": "./static/js/2.7bbfbecd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7bbfbecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265a3b0486e1884463bb",
+    "url": "./static/js/main.d4550b70.chunk.js"
+  },
+  {
+    "revision": "ce88a853913368144a1b2f161e25dae9",
+    "url": "./static/js/main.d4550b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a87b135236f9750cf16",
+    "url": "./static/js/runtime-main.dd4e3f12.js"
+  }
+]);
